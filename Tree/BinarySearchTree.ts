310d3c75615a37840cb897164b8ccf9a9c2aef69
jlm <jianglimin@gmail.com>
2023-02-20 20:06:19 +0800

    遍历

--- Tree/BinarySearchTree.ts
+++ Tree/BinarySearchTree.ts
@@ -1,3 +1,4 @@
+import { btPrint } from "hy-algokit";
 class TreeNode<T> {
   value: T;
   left: TreeNode<T> | null = null;
@@ -11,5 +12,105 @@ class TreeNode<T> {
 class BinarySearchTree<T> {
   private root: TreeNode<T> | null = null;
 
-  insert(value: T) {}
+  print() {
+    btPrint(this.root);
+  }
+
+  insert(value: T) {
+    const newNode = new TreeNode(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode<T>(root: TreeNode<T>, newNode: TreeNode<T>) {
+    if (root.value > newNode.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
+
+  preOrderTraverse() {
+    this.preOrderTraverseNode(this.root);
+  }
+
+  private preOrderTraverseNode(node: TreeNode<T> | null) {
+    if (node) {
+      console.log(node.value);
+      this.preOrderTraverseNode(node.left);
+      this.preOrderTraverseNode(node.right);
+    }
+  }
+
+  inOrderTraverse() {
+    this.inOrderTraverseNode(this.root);
+  }
+
+  private inOrderTraverseNode(node: TreeNode<T> | null) {
+    if (node) {
+      this.inOrderTraverseNode(node.left);
+      console.log(node.value);
+      this.inOrderTraverseNode(node.right);
+    }
+  }
+
+  postOrderTraverse() {
+    this.postOrderTraverseNode(this.root);
+  }
+
+  private postOrderTraverseNode(node: TreeNode<T> | null) {
+    if (node) {
+      this.postOrderTraverseNode(node.left);
+      this.postOrderTraverseNode(node.right);
+      console.log(node.value);
+    }
+  }
+
+  levelOrderTraverse() {
+    if (!this.root) return;
+    const queue: TreeNode<T>[] = [];
+    queue.push(this.root);
+    while (queue.length > 0) {
+      const current = queue.shift()!;
+      console.log(current.value);
+      if (current.left) {
+        queue.push(current.left);
+      }
+      if (current.right) {
+        queue.push(current.right);
+      }
+    }
+  }
 }
+
+const bst = new BinarySearchTree<number>();
+bst.insert(11);
+bst.insert(7);
+bst.insert(15);
+bst.insert(5);
+bst.insert(3);
+bst.insert(9);
+bst.insert(8);
+bst.insert(10);
+bst.insert(13);
+bst.insert(12);
+bst.insert(14);
+bst.insert(20);
+bst.insert(18);
+bst.insert(25);
+bst.insert(6);
+
+bst.print();
+bst.levelOrderTraverse();
